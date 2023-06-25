d59156a6a54bbe56bc05dc3354da2ce18e230e79
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2023-06-24 19:17:48 -0500

    Made better column alignment and provided a way to reset to initial state when all restaurants are removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const STORAGE_KEY = 'restaurants';
+const STORAGE_KEY = "restaurants";
 
 interface Restaurant {
     name: string;
     weight: number;
 }
 
-function App() {
+const MAX_LENGTH = 20;
+const INITIAL_STATE = [
+    { name: "Hotbox", weight: 0.5 },
+    { name: "Paramount", weight: 0.5 },
+    { name: "Eugene's", weight: 0.5 },
+    { name: "Marty's PM", weight: 0.5 },
+    { name: "Makarios", weight: 0.5 },
+    { name: "Saw's", weight: 0.5 },
+    { name: "Red Pearl", weight: 0.5 },
+    { name: "El Barrio", weight: 0.5 },
+    { name: "Giuseppe's", weight: 0.5 },
+    { name: "Rougaroux", weight: 0.5 },
+];
+
+const App = () => {
     const [choice, setChoice] = useState("");
     const [newName, setNewName] = useState("");
     const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
@@ -19,48 +33,43 @@ function App() {
         if (store) {
             setRestaurants(JSON.parse(store));
         } else {
-            setRestaurants([
-                { name: "Hotbox", weight: 0.5 },
-                { name: "Paramount", weight: 0.5 },
-                { name: "Marty's PM", weight: 0.5 },
-                { name: "Saw's", weight: 0.5 },
-                { name: "Jack Brown's", weight: 0.5 },
-                { name: "Eugene's", weight: 0.5 },
-                { name: "Hattie B's", weight: 0.5 },
-                { name: "Giuseppe's", weight: 0.5 }
-            ]);
+            setRestaurants(INITIAL_STATE);
         }
     }, []);
 
     useEffect(() => {
-        if (!restaurants.length) return;
+        if (!restaurants.length) {
+            localStorage.setItem(STORAGE_KEY, JSON.stringify(INITIAL_STATE));
+            setRestaurants(INITIAL_STATE);
+            return;
+        }
 
         localStorage.setItem(STORAGE_KEY, JSON.stringify(restaurants));
     }, [restaurants]);
 
     const handleAddRestaurant = () => {
         if (!newName) return;
-        setRestaurants([...restaurants, { name: newName, weight: 0.5 }]);
+        setRestaurants([...restaurants, { name: newName.substring(0, MAX_LENGTH), weight: 0.5 }]);
         setNewName("");
     };
 
     const handleRemoveRestaurant = (name: string) => {
-        setRestaurants(restaurants.filter(rs => rs.name !== name));
+        setRestaurants(restaurants.filter((rs) => rs.name !== name));
     };
 
     const handleAdjustWeights = (e: React.ChangeEvent<HTMLInputElement>) => {
         const temp = Array.from(restaurants);
-        const index = temp.findIndex(rs => rs.name === e.target.name);
+        const index = temp.findIndex((rs) => rs.name === e.target.name);
         temp[index].weight = e.target.valueAsNumber;
         setRestaurants(temp);
     };
 
     const handleReset = () => {
-        setRestaurants(restaurants.map(rs => ({ ...rs, weight: 0.5 })));
+        setRestaurants(restaurants.map((rs) => ({ ...rs, weight: 0.5 })));
     };
 
     const calculateRandomChoice = () => {
-        const allChoices = restaurants.map(rs => new Array(Math.round(rs.weight * 20)).fill(rs.name)).flat();
+        const allChoices = restaurants.map((rs) => new Array(Math.round(rs.weight * 20)).fill(rs.name)).flat();
         const randomChoice = allChoices[Math.floor(Math.random() * allChoices.length)];
         setChoice(randomChoice);
     };
@@ -74,32 +83,57 @@ function App() {
     return (
         <div className="App">
             <div>
-                <button onClick={handleReset}>Reset</button>
-                <button onClick={calculateRandomChoice}>
-                    {!choice ? (
-                        "Random choice!"
-                    ) : (
-                        <span>
-                            <strong>{choice}</strong>. Choose again?
-                        </span>
-                    )}
-                </button>
+                <button onClick={handleReset}>Reset weights</button>
+                <button onClick={calculateRandomChoice}>{!choice ? "Random choice!" : "Try again?"}</button>
+                <h1>
+                    <strong>{choice}</strong>
+                </h1>
             </div>
 
             <div>
-                <input placeholder="Add your restaurant here!" onKeyDown={e => handleSubmitOnEnter(e)} value={newName} onChange={e => setNewName(e.target.value)} type="text" />
-                <button style={{ color: "#00ff33" }} onClick={handleAddRestaurant}>+1</button>
+                <input
+                    placeholder="Add your restaurant here!"
+                    onKeyDown={(e) => handleSubmitOnEnter(e)}
+                    value={newName}
+                    onChange={(e) => setNewName(e.target.value)}
+                    type="text"
+                    maxLength={MAX_LENGTH}
+                />
+                <span className="smol">
+                    {newName.length}/{MAX_LENGTH}
+                </span>
+                <button style={{ color: "#00ff33" }} onClick={handleAddRestaurant}>
+                    +1
+                </button>
             </div>
 
             {restaurants.map((rs, i) => (
-                <div key={`restaurant-div-${i}`}>
-                    <label className="center-vert">{rs.name}</label>
-                    <input className="center-vert" onChange={handleAdjustWeights} name={rs.name} type="range" step={0.05} value={rs.weight} max={1} />
-                    <span className="center-vert" onClick={() => handleRemoveRestaurant(rs.name)} style={{ color: "red", fontSize: "1.25rem" }}>X</span>
+                <div className="row" key={`restaurant-div-${i}`}>
+                    <div className="col-left">
+                        <label>{rs.name}</label>
+                    </div>
+                    <div className="col-right">
+                        <input
+                            className="center-vert"
+                            onChange={handleAdjustWeights}
+                            name={rs.name}
+                            type="range"
+                            step={0.05}
+                            value={rs.weight}
+                            max={1}
+                        />
+                        <span
+                            className="center-vert"
+                            onClick={() => handleRemoveRestaurant(rs.name)}
+                            style={{ color: "red", fontSize: "1.25rem" }}
+                        >
+                            X
+                        </span>
+                    </div>
                 </div>
             ))}
         </div>
     );
-}
+};
 
 export default App;
